d211eb5b3544e6559840531ec254837415fcd937
Akshit Jain <akshitjain434303@gmail.com>
2025-01-08 22:59:10 +0530

    Initial commit: Add React app with base structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <main>
+    <h1>$400<span>.00</span></h1>
+    <form>
+      <div className="basic">
+      <input type="text" placeholder={'samsung tv'}/>
+      <input type="datetime-local"/>
+      </div>
+      <div>
+      <input type="text" placeholder={'description'}/>
+      </div>
+      <button type="submit">Add new transaction</button>
+    </form>
+    <div className='transactions'>
+      <div className='transaction'>
+        <div className='left'>
+          <div className="name">New Tv</div>
+          <div className="description">New Tv time</div>
+
+
+        </div>
+        <div className='right'>
+        <div className="price red">-$500</div>
+        <div className="datetime">2025-01-07 15:45</div>
+
+        </div>
+        
+      </div>
+      <div className='transaction'>
+        <div className='left'>
+          <div className="name">Gig Job</div>
+          <div className="description">New Tv time</div>
+
+
+        </div>
+        <div className='right'>
+        <div className="price green">+$400</div>
+        <div className="datetime">2025-01-07 15:45</div>
+
+        </div>
+        
+      </div>
+      <div className='transaction'>
+        <div className='left'>
+          <div className="name">iphone</div>
+          <div className="description">New Tv time</div>
+
+
+        </div>
+        <div className='right'>
+        <div className="price red">-$500</div>
+        <div className="datetime">2025-01-07 15:45</div>
+
+        </div>
+        
+      </div>
     </div>
+   </main>
   );
 }
 
